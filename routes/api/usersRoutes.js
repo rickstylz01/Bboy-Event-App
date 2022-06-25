dbcc66edd08b86c41a3c2ccf09aea4004d79d156
Ricardo Maya <rickmaya@gmail.com>
2022-06-25 16:20:50 -0500

    create user route and controller to login using passport and jwt to verify user credentials

--- routes/api/usersRoutes.js
+++ routes/api/usersRoutes.js
@@ -5,4 +5,9 @@ const UserController = require('../../controllers/usersControllers');
 // @route POST api/users/register
 // @desc Register user
 // @access Public
-router.post("/register", UserController.register)
+router.post("/register", UserController.register);
+
+// @route POST api/users/login
+// @desc Login user and return JWT token
+// @access Public
+router.post("/login", UserController.login);
