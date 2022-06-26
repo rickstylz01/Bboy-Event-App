089a3074e0ea793765e32ee185ffb1ab6bfd2164
Ricardo Maya <rickmaya@gmail.com>
2022-06-26 14:27:59 -0500

    create register and login components

---
+++ client/src/components/auth/Login.js
@@ -0,0 +1,109 @@
+import React, { useState } from 'react';
+import { Link } from "react-router-dom";
+
+const Register = () => {
+  const [state, setState] = useState({
+    name: "",
+    email: "",
+    password: "",
+    password2: "",
+    errors: {}
+  });
+
+  const onChange = (e) => {
+    setState({ [e.target.id]: e.target.value });
+  };
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+
+    const newUser = {
+      name: state.name,
+      email: state.email,
+      password: state.password,
+      password2: state.password2
+    };
+
+    console.log(newUser);
+  };
+
+  const { errors } = state.errors;
+
+  return(
+    <div className="container">
+      <div className="row">
+        <div className="col s8 offset-s2">
+          <Link to="/" className="btn-flat waves-effect">
+            <i className="material-icons">keyboard_backspace</i> Back to home
+          </Link>
+          <div className="col s12" style={{ paddingLeft: "11.250px"}}>
+            <h4>
+              <b>Register</b> below
+            </h4>
+            <p className="grey-text text-darken-1">
+              Already have an account? <Link to="/login">Log in</Link>
+            </p>
+          </div>
+          <form noValidate onSubmit={onSubmit}>
+            <div className="input-field col s12">
+              <input
+                onChange={onChange}
+                value={state.name}
+                error={errors.name}
+                id="name"
+                type="text"
+              />
+              <label htmlFor="name">Name</label>
+            </div>
+            <div className="input-field col s12">
+              <input
+                onChange={onChange}
+                value={state.email}
+                error={errors.email}
+                id="name"
+                type="text"
+              />
+              <label htmlFor="email">Email</label>
+            </div>
+            <div className="input-field col s12">
+              <input
+                onChange={onChange}
+                value={state.password}
+                error={errors.password}
+                id="name"
+                type="text"
+              />
+              <label htmlFor="password">Password</label>
+            </div>
+            <div className="input-field col s12">
+              <input
+                onChange={onChange}
+                value={state.password2}
+                error={errors.password2}
+                id="name"
+                type="text"
+              />
+              <label htmlFor="password2">Confirm Password</label>
+            </div>
+            <div className="col s12" style={{ paddingLeft: "11.250px" }}>
+              <button
+                style={{
+                  width: "150px",
+                  borderRadius: "3px",
+                  letterSpacing: "1.5px",
+                  marginTop: "1rem"
+                }}
+                type="submit"
+                className="btn btn-large waves-effect waves-light hoverable blue accent-3"
+              >
+                Sign Up
+              </button>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Register;
