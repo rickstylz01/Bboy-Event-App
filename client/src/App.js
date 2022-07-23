b276de79b09dcbabc9d6e066460d482a62078e8a
Ricardo Maya <rickmaya@gmail.com>
2022-07-23 15:07:30 -0500

    import Provider and store components. create directories for actions and reducers along with files authActions/reducers index store types and errorReducers

--- client/src/App.js
+++ client/src/App.js
@@ -6,15 +6,20 @@ import Landing from "./components/layout/Landing";
 import Register from "./components/auth/Register";
 import Login from "./components/auth/Login";
 
+import { Provider } from "react-redux";
+import store from "./store";
+
 const App = () => {
   return (
     <div className="App">
-      <Navbar/>
-      <Routes>
-        <Route exact path="/" element={<Landing />} />
-        <Route exact path="/register" element={<Register />} />
-        <Route exact path="/login" element={<Login />} />
-      </Routes>
+      <Provider store={store}>
+        <Navbar/>
+        <Routes>
+          <Route exact path="/" element={<Landing />} />
+          <Route exact path="/register" element={<Register />} />
+          <Route exact path="/login" element={<Login />} />
+        </Routes>
+      </Provider>
     </div>
   );
 }
